0cbdde32f5b2f9e5f0290b3b3849b3073f9a1778
Chey <ctenmanikdavid@gmail.com>
2023-07-29 14:32:50 +0000

    Make backup of env.py from root, add keys and values to environment variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -2,10 +2,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.isfile("env.py"):
-    import env
 
-# development = os.environ.get("DEVELOPMENT", False)
+development = os.environ.get("DEVELOPMENT", False)
+
 
 """
 Django settings for django_todo project.
@@ -34,10 +33,16 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "default-development-secret-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
+if development:
+    ALLOWED_HOSTS = [
+        "localhost",
+        "8000-cheyennemda-cheyfsfhell-o9tups2qexr.ws-eu102.gitpod.io",
+    ]
+else:
+    ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
-ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
 # Application definition
 
@@ -85,15 +90,17 @@ WSGI_APPLICATION = "django_todo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if development:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
-# else:
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+if development:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
